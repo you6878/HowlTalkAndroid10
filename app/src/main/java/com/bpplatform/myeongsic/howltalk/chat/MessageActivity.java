f51f7f2097f23f35d60a34f2454d1bae815eba2c
MyeongSic <you6878@icloud.com>
2017-10-02 23:41:13 +0900

    init

---
+++ app/src/main/java/com/bpplatform/myeongsic/howltalk/chat/MessageActivity.java
@@ -0,0 +1,83 @@
+package com.bpplatform.myeongsic.howltalk.chat;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.bpplatform.myeongsic.howltalk.R;
+import com.bpplatform.myeongsic.howltalk.model.ChatModel;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MessageActivity extends AppCompatActivity {
+
+    private String destinatonUid;
+    private Button button;
+    private EditText editText;
+
+    private String uid;
+    private String chatRoomUid;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message2);
+        uid = FirebaseAuth.getInstance().getCurrentUser().getUid();  //채팅을 요구 하는 아아디 즉 단말기에 로그인된 UID
+        destinatonUid = getIntent().getStringExtra("destinationUid"); // 채팅을 당하는 아이디
+        button = (Button) findViewById(R.id.messageActivity_button);
+        editText = (EditText) findViewById(R.id.messageActivity_editText);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ChatModel chatModel = new ChatModel();
+                chatModel.users.put(uid,true);
+                chatModel.users.put(destinatonUid,true);
+
+
+
+
+                if(chatRoomUid == null){
+                    FirebaseDatabase.getInstance().getReference().child("chatrooms").push().setValue(chatModel);
+                }else {
+
+                    ChatModel.Comment comment = new ChatModel.Comment();
+                    comment.uid = uid;
+                    comment.message = editText.getText().toString();
+                    FirebaseDatabase.getInstance().getReference().child("chatrooms").child(chatRoomUid).child("comments").push().setValue(comment);
+                }
+
+
+            }
+        });
+        checkChatRoom();
+
+
+    }
+
+    void  checkChatRoom(){
+
+        FirebaseDatabase.getInstance().getReference().child("chatrooms").orderByChild("users/"+uid).equalTo(true).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot item : dataSnapshot.getChildren()){
+                    ChatModel  chatModel = item.getValue(ChatModel.class);
+                        if(chatModel.users.containsKey(destinatonUid)){
+                            chatRoomUid = item.getKey();
+                        }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+}
